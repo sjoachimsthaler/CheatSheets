3818eb1d236c20938b1e732b6b0e7fa711a0341c
Stefan Joachimsthaler <16165980+sjoachimsthaler@users.noreply.github.com>
2023-02-03 00:01:20 +0100

    Create ssl.ps1

---
+++ ssl/ssl.ps1
@@ -0,0 +1,23 @@
+$subject = "/CN=test.home"
+$dnsAndIps = "subjectAltName=DNS:your-dns.record,IP:257.10.10.1"
+
+
+Write-Output $dnsAndIps
+
+openssl genrsa -aes256 -out ca-key.pem 4096
+
+openssl req -new -x509 -sha256 -days 3650 -key ca-key.pem -out ca.pem
+
+openssl genrsa -out cert-key.pem 4096
+
+openssl req -new -sha256 -subj $subject -key cert-key.pem -out cert.csr
+
+Write-Output "Create cnf"
+Write-Output $dnsAndIps >> extfile.cnf
+
+Write-Output "Create cert.pem"
+openssl x509 -req -sha256 -days 3650 -in cert.csr -CA ca.pem -CAkey ca-key.pem -out cert.pem -extfile extfile.cnf -CAcreateserial
+
+Write-Output "Create cert.pfx"
+openssl rsa -in cert-key.pem -out private.key
+openssl pkcs12 -export -out certificate.pfx -inkey private.key -in cert.pem -certfile cert.pem
